e1aebde902ef16e25807176ef9ec846688e7fc1a
Gaiypovv <eldargaiypov@gmail.com>
2024-02-18 17:30:35 +0600

    Initial Commit

---
+++ Auth_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Auth_App
+//
+//  Created by Eldar Gaiypov on 18/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
