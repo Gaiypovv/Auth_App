087f14b4500aff73eb37cd3c5113ffefa14bd86a
Gaiypovv <eldargaiypov@gmail.com>
2024-02-19 23:02:18 +0600

    updated the LogoutAlertView with new elements

--- Auth_App/View/WelcomeView/LogoutAlertView.swift
+++ Auth_App/View/WelcomeView/LogoutAlertView.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import SnapKit
 
 class LogoutAlertView: UIView {
     
@@ -19,13 +18,92 @@ class LogoutAlertView: UIView {
         return label
     }()
     
+    lazy var subtitleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 15)
+        label.text = "Are you sure?"
+        label.textColor = .darkGray
+        label.textAlignment = .center
+        label.numberOfLines = 1
+        return label
+    }()
+
+    lazy var yesButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Yes, sure thing!", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .black
+        button.layer.cornerRadius = 12
+        return button
+    }()
+
+    lazy var noButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("No, keep going", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 18)
+        button.setTitleColor(.black, for: .normal)
+        return button
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setupUI()
     }
 
     required init?(coder: NSCoder) {
         super.init(coder: coder)
+        setupUI()
     }
     
+    private func setupUI() {
+
+        backgroundColor = .white
+        layer.cornerRadius = 12
+        layer.masksToBounds = true
+
+        addSubview(titleLabel)
+        addSubview(subtitleLabel)
+        addSubview(yesButton)
+        addSubview(noButton)
+
+        titleLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalToSuperview().inset(15)
+            make.left.equalToSuperview().inset(15)
+            make.right.equalToSuperview().inset(15)
+            make.width.height.equalTo(40)
+        }
+        
+        subtitleLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(titleLabel.snp.bottom).offset(5)
+            make.left.equalToSuperview().inset(15)
+            make.right.equalToSuperview().inset(15)
+            make.width.height.equalTo(40)
+        }
+        
+        yesButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(subtitleLabel.snp.bottom).offset(15)
+            make.left.equalToSuperview().inset(15)
+            make.right.equalToSuperview().inset(15)
+            make.width.height.equalTo(45)
+        }
+        
+        noButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(yesButton.snp.bottom).offset(15)
+            make.left.equalToSuperview().inset(15)
+            make.right.equalToSuperview().inset(15)
+            make.width.height.equalTo(45)
+        }
+        
+        setNeedsUpdateConstraints()
+    }
+
+    override func updateConstraints() {
+        super.updateConstraints()
+    }
 }
 
